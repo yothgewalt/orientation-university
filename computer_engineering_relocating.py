c7cec08b34b7c28d58b279ad960d0e933dafcbfe
Yothgewalt <eine.schlacht@gmail.com>
2023-06-25 17:20:35 +0700

    (refactor): minimize the code and not use classes.

--- computer_engineering_relocating.py
+++ computer_engineering_relocating.py
@@ -6,45 +6,35 @@ GRADES = {
     "F": 0.00
 }
 
-class Student:
-    def __init__(
-            self, student_number: int,
-            gpx: float, computer_enginnering_grade: str,
-            calculas_one_grade: str, calculas_two_grade: str
-        ) -> None:
-        self.student_number = student_number
-        self.gpx = gpx
-        self.computer_enginnering_grade = computer_enginnering_grade.upper()
-        self.calculas_one_grade = calculas_one_grade.upper()
-        self.calculas_two_grade = calculas_two_grade.upper()
-
 students_data = []
 for _ in range(2):
     student_number, gpx, computer_enginnering_grade, calculas_one_grade, calculas_two_grade = input().split()
-    student = Student(int(student_number), float(gpx), computer_enginnering_grade, calculas_one_grade, calculas_two_grade)
+    student = {
+        "student_number": int(student_number),
+        "gpx": float(gpx),
+        "computer_enginnering_grade": computer_enginnering_grade.upper(),
+        "calculas_one_grade": calculas_one_grade.upper(),
+        "calculas_two_grade": calculas_two_grade.upper()
+    }
     students_data.append(student)
 
 passed_first_criterion = [
     student for student in students_data if
-    student.computer_enginnering_grade == "A" and
-    GRADES[student.calculas_one_grade] >= 2.00 and
-    GRADES[student.calculas_two_grade] >= 2.00
+    student["computer_enginnering_grade"] == "A" and
+    GRADES[student["calculas_one_grade"]] >= 2.00 and
+    GRADES[student["calculas_two_grade"]] >= 2.00
 ]
 
-if len(passed_first_criterion) > 1:
+if passed_first_criterion:
     passed_second_criterion = sorted(
         passed_first_criterion,
         key=lambda student: (
-            student.gpx,
-            student.calculas_one_grade,
-            student.calculas_two_grade
+            student["gpx"],
+            student["calculas_one_grade"],
+            student["calculas_two_grade"]
         ), reverse=True
     )
-
     highest_student = passed_second_criterion[0]
-
-    print(highest_student.student_number if highest_student else None)
-elif len(passed_first_criterion) == 1:
-    print(passed_first_criterion[0].student_number)
+    print(highest_student["student_number"])
 else:
     print(None)
