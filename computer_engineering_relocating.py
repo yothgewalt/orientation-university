ed2f61b2c2e7f2ed75608255af90d09fb547911d
Yothgewalt <eine.schlacht@gmail.com>
2023-06-26 10:32:14 +0700

    fixes(bug): cannot conditionaly both student

--- computer_engineering_relocating.py
+++ computer_engineering_relocating.py
@@ -6,7 +6,7 @@ GRADES = {
     "F": 0.00
 }
 
-students_data = []
+students = []
 for _ in range(2):
     student_number, gpx, computer_enginnering_grade, calculas_one_grade, calculas_two_grade = input().split()
     student = {
@@ -16,10 +16,10 @@ for _ in range(2):
         "calculas_one_grade": calculas_one_grade.upper(),
         "calculas_two_grade": calculas_two_grade.upper()
     }
-    students_data.append(student)
+    students.append(student)
 
 passed_first_criterion = [
-    student for student in students_data if
+    student for student in students if
     student["computer_enginnering_grade"] == "A" and
     GRADES[student["calculas_one_grade"]] >= 2.00 and
     GRADES[student["calculas_two_grade"]] >= 2.00
@@ -34,7 +34,34 @@ if passed_first_criterion:
             student["calculas_two_grade"]
         ), reverse=True
     )
-    highest_student = passed_second_criterion[0]
-    print(highest_student["student_number"])
+
+    highest_student_grade = passed_second_criterion[0]
+
+    if len(passed_second_criterion) > 1:
+        lowest_student_grade = passed_second_criterion[1]
+
+        same_conditionly: bool = False
+        if highest_student_grade["gpx"] == lowest_student_grade["gpx"]:
+            same_conditionly = True
+        else:
+            same_conditionly = False
+
+        if highest_student_grade["calculas_one_grade"] == lowest_student_grade["calculas_one_grade"]:
+            same_conditionly = True
+        else:
+            same_conditionly = False
+
+        if highest_student_grade["calculas_two_grade"] == lowest_student_grade["calculas_two_grade"]:
+            same_conditionly = True
+        else:
+            same_conditionly = False
+
+        if same_conditionly:
+            print("Both")
+        else:
+            print(None)
+    else:
+        print(highest_student_grade["student_number"])
+
 else:
     print(None)
